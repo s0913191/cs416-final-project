6a8d08b8b133097d7f09d1323077a8c86320d51c
s0913191 <s0913191@gmail.com>
2023-07-26 22:37:00 -0400

    Corrected annotation

--- src/annotation.js
+++ src/annotation.js
@@ -1,58 +1,57 @@
+const width = 1200;
+const height = 800;
+const margin = { top: 20, right: 20, bottom: 30, left: 100 };
+const graphWidth = width - margin.left - margin.right;
+const graphHeight = height - margin.top - margin.bottom;
+
 const annotations = [
     {
         note: {
-            label: "Terrorist Attack in NYC",
-            title: "2001"
+            label: "Terrorist Attacks",
+            title: "2001",
+            wrap:300
         },
-        //x: 100,
-        //y: 50,
-        data: { year: 2001, travelers: 27990385},
+        data: { year: 2001, travelers: 27990385 },
         dy: 100,
-        dx: 100,
+        dx: 0,
     }, {
         note: {
             label: "Global Financial Crisis",
-            title: "2008"
+            title: "2008",
+            wrap:300
         },
-        data: { year: 2008, travelers: 33390025},
-        //x: 400,
-        //y: 200,
-        dy: 100,
-        dx: 100
+        data: { year: 2008, travelers: 33390025 },
+        dy: -50,
+        dx: -50
     }, {
         note: {
-            label: "Donald Trump was elected",
-            title: "2016"
+            label: "Trump & Brexit",
+            title: "2016",
+            wrap:300
         },
-        data: { year: 2016, travelers: 48524355},
-        //x: 500,
-        //y: 100,
+        data: { year: 2016, travelers: 48524355 },
         dy: 100,
-        dx: 100
+        dx: 0
     }, {
         note: {
-            label: "COVID-19 worldwide pandemic",
-            title: "2020"
+            label: "COVID-19",
+            title: "2020",
+            wrap:300
         },
-        data: { year: 2020, travelers: 11257292},
-        //x: 600,
-        //y: 100,
-        dy: 100,
-        dx: 100
+        data: { year: 2020, travelers: 11257292 },
+        dy: 50,
+        dx: 0
     }
-].map(function (d) { d.color = "#E8336D"; return d })
+].map(function (d) { d.color = "#E8336D"; return d });
 
-
-const x = d3.scaleTime().domain([2000, 2022]).range([0, 1500]);
-const y = d3.scaleLinear().domain([0, 52203108]).range([770, 0]);
-
-console.log(x(2001),y(27990385));
+const xScale = d3.scaleTime().domain([2000, 2022]).range([margin.left, width - margin.right]);
+const yScale = d3.scaleLinear().domain([0, 52203108]).range([height - margin.bottom, margin.top]);
 
 const makeAnnotations = d3.annotation()
     .type(d3.annotationCalloutCircle)
     .accessors({
-        x: d => x(d.year),
-        y: d => y(d.travelers)
+        x: d => xScale(d.year),
+        y: d => yScale(d.travelers)
     })
     .accessorsInverse({
         date: function year(d) {
